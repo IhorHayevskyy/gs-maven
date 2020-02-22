451c333239127c6c213dbe4ab181922af17e2875
IhorHayevskyy <gidgid75@gmail.com>
2020-02-22 16:46:12 +0200

    time

--- initial/src/main/java/hello/HelloWorld.java
+++ initial/src/main/java/hello/HelloWorld.java
@@ -2,7 +2,9 @@ package hello;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		Greeter greeter = new Greeter();
-		System.out.println(greeter.sayHello());
-	}
+    LocalTime currentTime = new LocalTime();
+    System.out.println("The current local time is: " + currentTime);
+    Greeter greeter = new Greeter();
+    System.out.println(greeter.sayHello());
+  }
 }
